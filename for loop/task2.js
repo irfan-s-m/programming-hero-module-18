402dec8449ef9659d62080221d07b64171c74b5a
irfan-s-m <know.cse@gmail.com>
2024-01-30 02:57:33 +0600

    third commit

---
+++ for loop/task2.js
@@ -0,0 +1,18 @@
+// all odd numbers between 61 to 100
+for (let i = 61; i <= 100; i += 2) {
+    console.log(i);
+}
+
+// // another method
+for (let i = 61; i <= 100; i++) {
+    if (i % 2 === 1) {
+        console.log(i)
+    }
+}
+
+// even numbers between 78 to 98
+for (let i = 78; i <= 98; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
---
+++ while loop/task2.js
@@ -0,0 +1,13 @@
+let i =61;
+while (i<=100) {
+    if (i%2==1) {
+        console.log(i)
+    }
+    i++;
+}
+
+let i=78;
+while(i<=98) {
+    console.log(i);
+    i+=2;
+}
